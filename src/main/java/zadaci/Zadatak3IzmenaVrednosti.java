b1f4ebc5bbc828fc63655e4ce93ef3893b1555af
sasans <sasans@hotmail.com>
2016-09-27 19:51:08 +0200

    Uradjen zadatak izmena vrednosti

--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,15 +1,24 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Knjiga;
+import model.Oblast;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by android on 27.9.16..
  */
 public class Zadatak3IzmenaVrednosti {
+
+    static Dao<Knjiga,Integer> knjigaDao;
+    static Dao<Oblast,Integer> oblastDao;
+
     public static void main(String[] args) {
 
         ConnectionSource connectionSource = null;
@@ -20,6 +29,25 @@ public class Zadatak3IzmenaVrednosti {
             //Upostavljamo konekciju sa bazom
             c = DriverManager.getConnection("jdbc:sqlite:knjigaOblast.db");
 
+
+            knjigaDao = DaoManager.createDao(connectionSource, Knjiga.class);
+            oblastDao = DaoManager.createDao(connectionSource, Oblast.class);
+
+
+
+            //izlistavanje svih oblasti i knjiga
+            List<Oblast> oblast = oblastDao.queryForAll();
+            for (Oblast o : oblast)
+                System.out.println(o.toString());
+
+            oblast = oblastDao.queryForEq(Oblast.POLJE_NAZIV, "Activity klasa");
+            for (Oblast obl : oblast){
+                obl.setPocetnaStrana(35);
+                oblastDao.update(obl);
+            }
+
+            for (Oblast o : oblast)
+                System.out.println("Pocetna strana " + o.getPocetnaStrana() + "Updatovane oblasti");
 
 
             //SQL naredbe koje zelimo da posaljemo bazi
